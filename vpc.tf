ac78d0ae754696f37f39a39fb66fd3e879a4a1e0
Matthew <80968027+MLaunch@users.noreply.github.com>
2021-10-21 17:19:56 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -20,7 +20,7 @@ resource "random_string" "suffix" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.53.0"
+  version = "3.10.0"
 
   name                 = "education-vpc"
   cidr                 = "10.0.0.0/16"
